edf4a4b1233d77499e16616ed286a1fceedc9f29
no-regret666 <RangelJara195@gmail.com>
2024-05-02 17:03:17 +0800

    java

---
+++ Practice/src/Practice/Assert.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class Assert {
+    public static void main(String[] args) {
+        int x = 0;
+        assert  x > 0;
+    }
+}
---
+++ Practice/src/Practice/Reflection/Class.java
@@ -0,0 +1,5 @@
+package Practice.Reflection;
+
+public class Class {
+
+}
